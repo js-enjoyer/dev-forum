3060c7484898b6be55e26e18a52ab5a640035603
js-enjoyer <vasilen.tihin@gmail.com>
2024-12-16 16:00:17 +0200

    Adding Home View
    Adding the home page with css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { AsideComponent } from './main/aside/aside.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from "./main/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AsideComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
