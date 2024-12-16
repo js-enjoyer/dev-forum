bb5b6248af9fac24e1fa06a778b20caefc55d90a
js-enjoyer <vasilen.tihin@gmail.com>
2024-12-16 16:38:29 +0200

    Adding search dropdown menu
    Added a dropdown menu below search bar for tag filtering with toggle functionality.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,13 +1,28 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RouterLink ],
+  imports: [ RouterLink, NgFor, NgIf ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  tags: string[] = [
+    'javascript', 'node.js', 'react', 'angular', 'vue.js', 'typescript', 
+    'html', 'css', 'python', 'django', 'flask', 'java', 'spring', 'c++', 'rust'
+  ];
 
+  // Selected tags
+  selectedTags: string[] = ['javascript', 'node.js', 'react'];
+
+  // Dropdown visibility state
+  dropdownVisible: boolean = false;
+
+  // Toggle dropdown visibility
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
 }
